846a6a4328b5f165e0079a33642abd9d44055c66
Sebastian Sturm <sebastian.sturm@doubleslash.de>
2022-07-22 21:02:10 +0200

    first try
    Signed-off-by: Sebastian Sturm <sebastian.sturm@doubleslash.de>

--- src/main/java/de/doubleslash/poker/player/logic/Strategy.java
+++ src/main/java/de/doubleslash/poker/player/logic/Strategy.java
@@ -5,6 +5,7 @@ import de.doubleslash.poker.player.data.Rank;
 import de.doubleslash.poker.player.data.Table;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Strategy {
 
@@ -46,7 +47,7 @@ public class Strategy {
    }
 
    private boolean hasRoyalFush(List<Card> cards){
-       cards.stream().sorted((c1, c2) -> c1.getRank().compareTo(c2.getRank()));
+      cards =  cards.stream().sorted((c1, c2) -> c1.getRank().compareTo(c2.getRank())).collect(Collectors.toList());
       if (cards.get(0).getRank() == Rank.ACE && cards.get(1).getRank() == Rank.KING && cards.get(2).getRank() == Rank.QUEEN && cards.get(3).getRank() == Rank.JACK && cards.get(4).getRank() == Rank.TEN) {
          return true;
       }
