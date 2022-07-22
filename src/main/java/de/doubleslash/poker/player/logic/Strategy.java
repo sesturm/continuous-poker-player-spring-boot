56359f958f645a236a449507096f188b4e6e8074
Sebastian Sturm <sebastian.sturm@doubleslash.de>
2022-07-22 20:47:33 +0200

    first try
    Signed-off-by: Sebastian Sturm <sebastian.sturm@doubleslash.de>

--- src/main/java/de/doubleslash/poker/player/logic/Strategy.java
+++ src/main/java/de/doubleslash/poker/player/logic/Strategy.java
@@ -1,6 +1,7 @@
 package de.doubleslash.poker.player.logic;
 
 import de.doubleslash.poker.player.data.Card;
+import de.doubleslash.poker.player.data.Rank;
 import de.doubleslash.poker.player.data.Table;
 
 import java.util.List;
@@ -16,19 +17,58 @@ public class Strategy {
       return false;
    }
 
-   private boolean hasFlush(List<Card> cards){
-      for (int i = 0; i < cards.size() - 1; i++) {
-         if (cards.get(i).getSuit() != cards.get(i + 1).getSuit()) {
-            return false;
+   private boolean hasStraight(List<Card> card){
+      for (int i = 0; i < card.size() - 3; i++) {
+         if (card.get(i).getRank() == card.get(i + 1).getRank() && card.get(i).getRank() == card.get(i + 2).getRank() && card.get(i).getRank() == card.get(i + 3).getRank()) {
+            return true;
          }
       }
-      return true;
+      return false;
    }
 
-   public int decide(final Table table) {
+   private boolean hasTriple(List<Card> card){
+      for (int i = 0; i < card.size() - 2; i++) {
+         if (card.get(i).getRank() == card.get(i + 1).getRank() && card.get(i).getRank() == card.get(i + 2).getRank()) {
+            return true;
+         }
+      }
+      return false;
+   }
 
-      System.out.println(table);
-      return 1;
+   private boolean hasTwoPair(List<Card> card){
+      for (int i = 0; i < card.size() - 1; i++) {
+         if (card.get(i).getRank() == card.get(i + 1).getRank()) {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   private boolean hasRoyalFush(List<Card> cards){
+      if (cards.get(0).getRank() == Rank.ACE && cards.get(1).getRank() == Rank.KING && cards.get(2).getRank() == Rank.QUEEN && cards.get(3).getRank() == Rank.JACK && cards.get(4).getRank() == Rank.TEN) {
+         return true;
+      }
+      return false;
+   }
+  public int decide(Table table){
+      List<Card> cards = table.getCommunityCards();
+     cards.addAll(table.getOwnPlayer().getCards());
+      if (hasRoyalFush(cards)) {
+         return 1;
+      }
+      if (hasStraight(cards)) {
+         return 2;
+      }
+      if (hasTriple(cards)) {
+         return 3;
+      }
+      if (hasTwoPair(cards)) {
+         return 4;
+      }
+      if (hasPair(cards)) {
+         return 5;
+      }
+      return 6;
    }
 
 }
